df6e1e1178476caeaa76db50af6c149cdfdf6f41
root <root@geilt.gateway.2wire.net>
2012-07-20 21:13:42 -0400

    Modified Posts Model

--- system/models/post.js
+++ system/models/post.js
@@ -12,9 +12,16 @@ var Mongoose 	= require('mongoose'),
  * Create the Model
  */
 var Post = new Mongoose.Schema({
+	name 		: {type : String, required : true},
 	author 		: {type  : ObjectId, ref : 'User'},
 	title 		: {type : String, required : true},
+	slug 		: {type : String, required : true, lowercase: true},
+	description	: {type : String, required : true},
+	keywords 	: {type : String,  required : true, lowercase: true},
 	content 	: {type : String, required : true},
+	excerpt		: {type : String, required : false},
+	status		: {type : String, enum : ['published', 'draft', 'private'], default: 'draft'},
+	created 	: {type : Date, default: Date.now},
 	published 	: {type : Date, default: Date.now},
 	modified 	: {type : Date, default: Date.now},
 	categories 	: [{type : ObjectId, ref : 'Category'}],
